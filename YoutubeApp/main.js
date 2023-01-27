1a742fac103532effebca0f6756cb3244293e927
Ravi Kumar <99811466+rao8969@users.noreply.github.com>
2023-01-27 16:47:45 +0530

    Add files via upload

---
+++ YoutubeApp/main.js
@@ -0,0 +1,38 @@
+//https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=kgf%202&key=[YOUR_API_KEY] 
+const API="AIzaSyCgDeLKaVVxYFZgi4xY3N4gwUg1We4XA9A"
+
+const searchVideos= async ()=>{
+    try {
+        const q=document.getElementById("query").value 
+
+        const res=await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${q}%202&key=${API}`)
+        const data=await res.json();
+        append(data.items)
+        console.log(data.items)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const append=(videos)=>{
+    const show_videos=document.getElementById("show_videos");
+    show_videos.innerText=null;
+
+    videos.forEach(({id:{videoId},snippet:{title}})=>{
+        let div=document.createElement("div");
+
+        let iframe=document.createElement("iframe")
+        iframe.src=`https://www.youtube.com/embed/${videoId}`
+         iframe.width="100%";
+         iframe.height="100%";
+        iframe.allow="fullscreen";
+
+
+        let name=document.createElement("h5")
+        name.innerText=title;
+
+        div.append(iframe,name);
+        show_videos.append(div);
+    })
+
+}
